0c711ad06decf3dd14b3462afb03d48511d73d06
Alysa Emilio <155709231+AliDevPh@users.noreply.github.com>
2024-02-15 12:34:05 +0800

    Add files via upload

---
+++ src/javaapplication7/winnertf.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication7;
+
+/**
+ *
+ * @author Alysa Emilio
+ */
+class winnertf {
+
+    static void setText(String call_of_Duty_Wins) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
